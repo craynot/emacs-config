bd704eba1291fcb8fdf0ef84707149e2f3759a4a
Nikita Alekseev <os.nikit.os@gmail.com>
2025-03-22 13:11:04 +0300

    go-mode customizable gofmt-command

--- init/init-go.el
+++ init/init-go.el
@@ -1,6 +1,10 @@
 ;;; init-go.el --- GO tools
 
 ;;; Commentary:
+;;
+;; Example .dir.locals.el for rewriting gofmt-command:
+;;
+;; ((go-mode . ((gofmt-command . "my-go-fmt.sh"))))
 
 ;;; Code:
 
@@ -8,16 +12,22 @@
   :ensure t
   :config
   (add-to-list 'auto-mode-alist '("\\.go$" . go-mode))
+  (add-to-list 'safe-local-variable-values '(gofmt-command . "my-go-fmt.sh"))
   :init
   (add-hook 'go-mode-hook #'(lambda ()
 			      (add-hook 'before-save-hook
-					#'gofmt-before-save
-					nil t)
-			      ;; Lsp jump-to
-			      (define-key go-mode-map (kbd "M-]")
-					  'lsp-bridge-find-def)
-			      (define-key go-mode-map (kbd "M-[")
-					  'lsp-bridge-find-def-return))))
+                                        #'(lambda ()
+                                            (when (eq major-mode 'go-mode)
+                                              (let ((gofmt-command (or (cdr (assoc 'gofmt-command
+                                                                                   (dir-locals-get-class-variables
+                                                                                    (dir-locals-find-file default-directory))))
+								       gofmt-command)))
+						(gofmt-before-save))
+                                              nil t)))))
+  :bind (:map go-mode-map
+              ("M-]" . lsp-bridge-find-def)
+              ("M-[" . lsp-bridge-find-def-return)
+              ("M-." . lsp-bridge-find-references)))
 
 (provide 'init-go)
 
